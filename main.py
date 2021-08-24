c1b40220115e0665f3ab40cca38dcee1960958a6
Marko Radovic <marko_radovic@outlook.com>
2021-08-24 11:07:19 +0200

    Finished python script

---
+++ main.py
@@ -0,0 +1,32 @@
+MORSE = {'A': '.-',     'B': '-...',   'C': '-.-.',
+        'D': '-..',    'E': '.',      'F': '..-.',
+        'G': '--.',    'H': '....',   'I': '..',
+        'J': '.---',   'K': '-.-',    'L': '.-..',
+        'M': '--',     'N': '-.',     'O': '---',
+        'P': '.--.',   'Q': '--.-',   'R': '.-.',
+        'S': '...',    'T': '-',      'U': '..-',
+        'V': '...-',   'W': '.--',    'X': '-..-',
+        'Y': '-.--',   'Z': '--..',
+
+        '0': '-----',  '1': '.----',  '2': '..---',
+        '3': '...--',  '4': '....-',  '5': '.....',
+        '6': '-....',  '7': '--...',  '8': '---..',
+        '9': '----.'
+        }
+
+text = input("Please enter text you want translated to morse code: ").upper()
+
+words = text.split()
+test_list = []
+output=""
+try:
+    for word in words:
+        for letter in word:
+            output += MORSE[letter]
+        output += " "
+except KeyError:
+    print("Please only enter letters and numbers")
+
+print(output)
+
+input("press ENTER to continue")
